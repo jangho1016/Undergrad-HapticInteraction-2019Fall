3d5deb25fde601e2d8aa3e907da119e3edcfd1ac
jangho1016 <dhwkdgh12@gmail.com>
2019-10-23 19:58:35 +0900

    edited

---
+++ 01_Arduino/02_1_Hello_Serial_Receive/02_1_Hello_Serial_Receive.ino
@@ -0,0 +1,31 @@
+int cnt1 = 0; //카운트 초기화
+
+void setup() {
+
+  Serial.begin(9600); //시리얼 넘버 초기화
+
+}
+
+ 
+
+// 1초마다 PC로 숫자를 보내는 예제
+
+void loop() {
+
+  char buf[255]; //buf 배열에 담음
+
+  sprintf(buf, "%d", cnt1); //프로세싱에 buf와 cnt 출력
+
+  // "%d" 를 "%c" 로 바꾸면 어떻게 될까요?
+
+  
+
+  Serial.println(buf); //시리얼의 buf를 출력
+
+  cnt1++; //카운트 증가
+
+  
+
+  delay(1000); //1초 후에
+
+}
